1ab0ba599c76b50fd181da1150a8a641e35983b7
Renê Schneider <rene.schneider@oqvestir.com.br>
2018-04-10 15:24:48 -0300

    Quick fix for main route test.

--- test/collava_web/controllers/page_controller_test.exs
+++ test/collava_web/controllers/page_controller_test.exs
@@ -2,7 +2,7 @@ defmodule CollavaWeb.PageControllerTest do
   use CollavaWeb.ConnCase
 
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    conn = get(conn, "/")
+    assert html_response(conn, 200) =~ "Hello Collava!"
   end
 end
