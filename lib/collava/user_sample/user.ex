dfea3c531c8e3d7e6cb9319f1d4be780e08734ef
Thales <thales.flores@icommgroup.com.br>
2018-04-10 18:25:01 -0300

    user schema

---
+++ lib/collava/user_sample/user.ex
@@ -0,0 +1,20 @@
+defmodule Collava.UserSample.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+
+  schema "users" do
+    field :age, :integer
+    field :name, :string
+
+    timestamps()
+  end
+
+  @doc false
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:name, :age])
+    |> validate_required([:name, :age])
+  end
+end
