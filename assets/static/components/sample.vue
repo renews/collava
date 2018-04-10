3ba8cfea0bd4b7ee651b16963de03a095c617ae0
Renê Schneider <rene.schneider@oqvestir.com.br>
2018-04-10 17:35:56 -0300

    Remove ELm, and ADD VUE + samples.

---
+++ assets/static/components/sample.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="vue-sample">
+    <h1>Vuechat</h1>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="sass">
+.vue-sample {
+  margin-left: auto;
+  margin-right: auto;
+  width: 800px;
+  h1 {
+    text-align: center;
+  }
+}
+</style>
